c41f3a56edb859c7cfea93af283980fe43664c3a
sp90 <dev@nomis.dk>
2016-05-21 19:25:35 +0200

    fix isloggedin error

--- api/isLoggedin.js
+++ api/isLoggedin.js
@@ -1,42 +1,33 @@
 // Include libs
+var _ = require('lodash');
 var jwt = require('jsonwebtoken');
 var MongoClient = require('mongodb').MongoClient;
-var _ = require('lodash');
 
 // Configs
 var gConfig = require('./_global-config');
+var gHelpers = require('./_global-helpers');
 
 function isLoggedin(req, res, next) {
 	var token = req.get('Authorization');
 
 	// Verify token
 	jwt.verify(token, gConfig.tokenSecret, function(err, decoded) {
-
 		if (!_.isNull(err)) {
-			return res.json({
-				'success': false,
-				'message': err.message
-			});
+			return res.json(gHelpers.errRes(err.message));
 		}
 
 		// Get user object
 		MongoClient.connect(gConfig.mongoHost, function(err, db) {
 			'use strict';
-			if(err) {
-				return res.json({
-					'success': false,
-					'message': 'DB connection failed'
-				});
+
+			if (!_.isNull(err)) {
+				return res.json(gHelpers.errRes('DB connection failed'));
 			}
 
 			var Users = db.collection('users');
-
 			Users.findOne({'email': decoded.email}, function(err, user) {
-				if(err) {
-					return res.json({
-						'success': false,
-						'message': 'Failed while trying to get the user'
-					});
+				if (!_.isNull(err)) {
+					return res.json(gHelpers.errRes('Failed while trying to get the user'));
 				}
 
 				// Bind user to the request session
