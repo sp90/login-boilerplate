b30a394cd87c8e0eb94fcf78d4cf29719fd71d10
sp90 <dev@nomis.dk>
2016-05-21 14:33:36 +0200

    prep for alternative auth methods

--- api/_global-config-example.js
+++ api/_global-config-example.js
@@ -15,6 +15,21 @@ if (env === 'staging' || env === 'production') {
 		mongoHost: 'mongodb://mongo:27017/test1',
 		sendgrid: {
 			apikey: 'somekey'
+		},
+		facebook : {
+			app_id: 'your-secret-clientID-here', // your App ID
+			app_secret: 'your-client-secret-here', // your App Secret
+			callback_url: 'http://somedomain.com/auth/facebook/callback'
+		},
+		twitter: {
+			app_key: 'your-consumer-key-here', // consumer_key
+			app_secret: 'your-client-secret-here', // consumer_secret
+			callback_url: 'http://somedomain.com/auth/twitter/callback'
+		},
+		google: {
+			app_id: 'your-secret-clientID-here', // client_id
+			app_secret: 'your-client-secret-here', // client_secret
+			callback_url: 'http://somedomain.com/auth/google/callback'
 		}
 	};
 } else {
@@ -27,6 +42,21 @@ if (env === 'staging' || env === 'production') {
 		mongoHost: 'mongodb://localhost:27017/test1',
 		sendgrid: {
 			apikey: 'somekey'
+		},
+		facebook : {
+			app_id: 'your-secret-clientID-here', // your App ID
+			app_secret: 'your-client-secret-here', // your App Secret
+			callback_url: 'http://localhost:8080/auth/facebook/callback'
+		},
+		twitter: {
+			app_key: 'your-consumer-key-here', // consumer_key
+			app_secret: 'your-client-secret-here', // consumer_secret
+			callback_url: 'http://localhost:8080/auth/twitter/callback'
+		},
+		google: {
+			app_id: 'your-secret-clientID-here', // client_id
+			app_secret: 'your-client-secret-here', // client_secret
+			callback_url: 'http://localhost:8080/auth/google/callback'
 		}
 	};
 }
