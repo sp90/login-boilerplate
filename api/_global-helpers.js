c956d1f746e808499bdda6036c83ca813d8c5cf5
sp90 <dev@nomis.dk>
2016-05-21 19:26:27 +0200

    improve error handler with optional data object

--- api/_global-helpers.js
+++ api/_global-helpers.js
@@ -1,9 +1,17 @@
+var _ = require('lodash');
+
 // Setup helpers
-function errRes(message) {
-	return {
+function errRes(message, xtraObj) {
+	var obj = {
 		'success': false,
 		'message': message
-	}
+	};
+
+	if (!_.isUndefined(xtraObj)) {
+		obj.data = xtraObj;
+	};
+
+	return obj;
 }
 
 // Export Rest routes 
