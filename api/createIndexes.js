d072424465487fa3af3f0babd9cb779caa87311e
sp90 <dev@nomis.dk>
2016-05-23 16:33:11 +0200

    fix failing indexing

--- api/createIndexes.js
+++ api/createIndexes.js
@@ -1,26 +1,35 @@
 // Prep vars
 var MongoClient = require('mongodb').MongoClient;
+var Promise = require("bluebird");
+
 var config = require('./_global-config');
 
 // Run once
-MongoClient.connect(config.mongoHost, function(err, db) {
-    'use strict';
-    if(err) throw err;
+function createIndexes() {
+	MongoClient.connect(config.mongoHost, function(err, db) {
+		'use strict';
+		if(err) throw err;
 
-	var Users = db.collection('users');
+		var Users = db.collection('users');
 
-	// Index user email
-	Users.createIndex(
-		{
-			'email': 1
-		}, 
-		{
-			unique: true
-		},
-		function(err, result) {
-			console.log("err: ", err);
-			console.log("result: ", result);
-		});
-	
-	db.close();
-});
+		// Index user email
+		Users.createIndex(
+			{
+				'email': 1
+			}, 
+			{
+				unique: true
+			},
+			function(err, result) {
+				if(err) { throw err; }
+
+				// Tell result
+				console.log("result: ", result);
+
+				// Close db
+				db.close();
+			});
+	});
+}
+
+createIndexes();
